5b50bb102a0933a1ac86dff3dcce922323343559
r n <quggoif@mail.ru>
2024-04-18 13:05:38 +0300

    Add AppShell, MainMenuPage and folders to implement the MVVM pattern

--- DndFightManagerMobileApp/DndFightManagerMobileApp/App.xaml.cs
+++ DndFightManagerMobileApp/DndFightManagerMobileApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace DndFightManagerMobileApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
---
+++ DndFightManagerMobileApp/DndFightManagerMobileApp/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using DndFightManagerMobileApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DndFightManagerMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(MainMenuPage), typeof(MainMenuPage));
+        }
+    }
+}
